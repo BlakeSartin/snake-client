58883867d2e872a419adcdd291fb6ac7bc04a337
Blake Sartin <blake.sartin8@gmail.com>
2022-01-19 22:50:17 +0000

    contant vars

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const MESSAGES = {
+  'q': 'Say:there is no god',
+  "e": 'Say:help',
+}
+
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES,
+};
