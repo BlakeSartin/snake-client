1906ff4a37afcd0299cc963fc6151ce764d8e6a2
Blake Sartin <blake.sartin8@gmail.com>
2022-01-19 21:49:34 +0000

    control c add

--- play.js
+++ play.js
@@ -1,25 +1,22 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
+const connect = require("./client");
 
-  conn.on("connect", () => {
-    console.log("succesfully connected!")
-  });
-
-  conn.on("data", (data) => {
-    console.log("received", data)
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
+const handleUserInput = function () {
+  if (key === "\u0003") {
+    process.exit();
+  }
 };
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
